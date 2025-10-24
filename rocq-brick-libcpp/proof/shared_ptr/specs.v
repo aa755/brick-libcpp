d64e43505db0ef8f80a9ed7ce4f9b6fb0957c290
Abhishek Anand (Monad Labs) <abhishek.anand.iitg@gmail.com>
2025-10-24 15:42:19 -0400

    added back payload ownership tracking to ensure destructor spec is sound. user can chose its own way to split ownerhsip betweent eh various shared ptr objects on the same payload. they can also resplit later

--- rocq-brick-libcpp/proof/shared_ptr/specs.v
+++ rocq-brick-libcpp/proof/shared_ptr/specs.v
@@ -17,7 +17,7 @@ Definition liftQ {PROP: bi} (p: Qp->PROP) (q:Q) : PROP :=
 
 Record CtrlBlockId : Set :=
   {
-    contenderLoc: gname;
+    contenderLocs: list gname; (* each stores a unit. N -> gname may be tricky for constructor proof *)
     dataLoc: ptr;
   }.
 
@@ -32,6 +32,9 @@ Definition ctrlBlockPtrOffset: offset. Proof. Admitted.
 
 Lemma maxContentionLb : 2^32 <= Npos maxContention. Proof. Admitted.
 Definition maxContentionQp := pos_to_Qp maxContention.
+
+Definition countLN {A : Type} (f : A -> bool) (l : list A) : N :=
+  lengthN (filter f l).
 
 
 Section specs.
@@ -53,57 +56,72 @@ Section specs.
 
   Context {hf:fracG () _Σ}.
 
+  (* just an execlusive token for each contenderid. can be defined with a simpler CMRA as fractionality is not needed. fgptsoQ has good automation support  *)
+  Definition copyConstrRight ctrlid contenderid : mpred :=
+    match nth_error (contenderLocs ctrlid) contenderid with
+    | Some g => fgptstoQ g 1 tt
+    | None => emp (* bad argument, contenderid > maxContention *)
+    end.
+      
+  Definition allContenderIds : list nat := (seq 0 (Pos.to_nat maxContention)).
+  
   (** Currently, we assume the control block is just an atomic counter.
       In reality, it is probably a struct. so move the atomicR to some defn ctrlBlockR *)
-  Definition SharedPtrR (cppty: type) (id: CtrlBlockId) (ownedPtr:ptr)  : Rep :=
+  Definition SharedPtrR (cppty: type) (id: CtrlBlockId) (Rpiece : nat -> Rep) (ownedPtr:ptr)  : Rep :=
     structR ("std::shared_ptr".<<Atype cppty>>) 1
     ** ownedPtrOffset |-> primR "cppty" 1 (Vptr ownedPtr)
     ** ctrlBlockPtrOffset |-> primR "cppty" 1 (Vptr (dataLoc id))
     ** [| ownedPtr<>nullptr |] (* use NullSharedPtr othewise *)
-    ** pureR (inv nroot
-                (Exists ctrVal:Z,
-                  (dataLoc id),, ctrOffset |-> atomic.R "long" 1 ctrVal
-                   ** fgptstoQ (contenderLoc id) (ctrVal # maxContention) tt
-                   ** (if (bool_decide (ctrVal = 0))
-                       then emp
-                       else dynAllocatedR cppty ownedPtr))).
+    ** [| lengthN (contenderLocs id) = Npos maxContention |]
+    ** pureR (inv nroot (Exists (ctrVal:N) (pieceOut : nat ->bool) ,
+         (dataLoc id),, ctrOffset |-> atomic.R "long" 1 (Z.of_N ctrVal)
+         ** ([∗ list] ctid ∈ allContenderIds,
+             if pieceOut ctid then copyConstrRight id ctid else ownedPtr |-> Rpiece ctid)
+         ** [| countLN pieceOut allContenderIds  = ctrVal |]
+         ** (if (bool_decide (ctrVal = 0))
+              then emp
+              else dynAllocatedR cppty ownedPtr))).
 
   Definition NullSharedPtrR (cppty: type) : Rep :=
     structR "shared_ptr<int>" 1
     ** ownedPtrOffset |-> primR (Tptr "cppty") 1 (Vptr nullptr)
     ** ctrlBlockPtrOffset |->  primR (Tptr "cppty") 1 (Vptr nullptr).
 
-  Definition copyConstrRight id : mpred :=
-    fgptstoQ (contenderLoc id) (1 # maxContention) tt.
 
   Definition Lstar (l: list mpred) : mpred := [∗ list] i ∈ l, i.
 
+  Definition allButFirstContenderId := (seq 1 (Pos.to_nat maxContention)).
   cpp.spec "std::shared_ptr<int>::shared_ptr<int, void>(int*)" as shp with (fun (this:ptr) =>
     \arg{p:ptr} "ownedPtr" (Vptr p)
     \pre{p} dynAllocatedR "int" p
+    \pre{Rpiece: nat -> Rep} [∗ list] ctid ∈ allButFirstContenderId,
+      p |-> Rpiece ctid
+
     \post Exists (ctrlBlockId: CtrlBlockId),
-       this |-> SharedPtrR "int"  ctrlBlockId p
-       ** Lstar (List.repeat (copyConstrRight ctrlBlockId) (Pos.to_nat maxContention -1))
+       this |-> SharedPtrR "int"  ctrlBlockId Rpiece p
+       ** ([∗ list] ctid ∈ allButFirstContenderId, copyConstrRight ctrlBlockId ctid)
        ).
 
-  (** move constructor *)
+  (** move constructor. the new object represents the same piece of ownership  *)
   cpp.spec "std::shared_ptr<int>::shared_ptr(std::shared_ptr<int>&&)" as shm with (fun (this:ptr) =>
     \arg{other:ptr} "other" (Vptr other)
-    \pre{ctrlBlockId ownedPtr} other |-> SharedPtrR "int" ctrlBlockId ownedPtr
+    \pre{ctrlBlockId ownedPtr Rpiece} other |-> SharedPtrR "int" ctrlBlockId Rpiece ownedPtr
     \post other  |-> NullSharedPtrR "int"
-          ** this |-> SharedPtrR "int"  ctrlBlockId ownedPtr).
+          ** this |-> SharedPtrR "int"  ctrlBlockId Rpiece ownedPtr).
 
 
   cpp.spec "std::shared_ptr<int>::~shared_ptr()" as shd1 with (fun (this:ptr) =>
     \with (null:bool)
-    \pre{(p:ptr) (sid: if null then unit else CtrlBlockId)}
-      this |-> (match null as b return (if b then unit else CtrlBlockId) -> Rep with
-                | false => fun sid=> SharedPtrR "int" sid p
+    \pre{(p:ptr) (sid: if null then unit else prod CtrlBlockId nat) Rpiece}
+      this |-> (match null as b return (if b then unit else prod CtrlBlockId nat) -> Rep with
+                | false => fun sid=>
+                             SharedPtrR "int" sid.1 Rpiece p
+                             ** Rpiece sid.2
                 | true => fun sid=> NullSharedPtrR "int"
                 end) sid
 
-    \post (match null as b return (if b then unit else CtrlBlockId) -> mpred with
-                | false => fun sid=> copyConstrRight sid
+    \post (match null as b return (if b then unit else prod CtrlBlockId nat) -> mpred with
+                | false => fun sid=> copyConstrRight sid.1 sid.2
                 | true => fun sid=> emp
                 end) sid).
 
@@ -117,16 +135,16 @@ Section specs.
   cpp.spec "std::shared_ptr<int>::shared_ptr(std::shared_ptr<int> const&)" as shc1
     with (fun (this:ptr) =>
     \arg{other:ptr} "other" (Vptr other)
-    \pre{id p}
-         other |-> SharedPtrR "int" id p
-         ** copyConstrRight id (* this will be returned by destructor *)
+    \pre{id ctid p Rpiece}
+         other |-> SharedPtrR "int" id Rpiece p
+         ** copyConstrRight id ctid (* this will be returned by destructor *)
     \post
-         this  |-> SharedPtrR "int" id p
-          ** other |-> SharedPtrR "int" id p
+         p|->Rpiece ctid ** this  |-> SharedPtrR "int" id Rpiece p
+          ** other |-> SharedPtrR "int" id Rpiece p
        ).
 
   (** Copy-ctor from null: produces another null shared_ptr.
-      No token is required. *)
+      No token is required. TODO: unify this spec with the spec above, using dependent types, as done in the destructor spec *)
   cpp.spec "std::shared_ptr<int>::shared_ptr(std::shared_ptr<int> const&)" as shc2
     with (fun (this:ptr) =>
     \arg{other:ptr} "other" (Vptr other)
@@ -153,36 +171,80 @@ Section specs.
 
   cpp.spec (SP_acc.::Nop function_qualifiers.Nc OOStar []) as shg with 
     (fun (this:ptr) =>
-       \prepost{id p} this |-> upcast_offset |-> SharedPtrR "int" id p
+       \prepost{id p Rpiece} this |-> upcast_offset |-> SharedPtrR "int" id Rpiece p
        \post[Vptr p] emp
        ).
 
-  #[global] Instance sharedR_typeptr_observe ty id (p:ptr) op
-    : Observe (type_ptr (Tnamed ("std::shared_ptr".<<Atype ty>>)) p) (p|->SharedPtrR ty id op):= _.
+  #[global] Instance sharedR_typeptr_observe ty id (p:ptr) op Rpiece
+    : Observe (type_ptr (Tnamed ("std::shared_ptr".<<Atype ty>>)) p) (p|->SharedPtrR ty id Rpiece op):= _.
 
+  Definition allPiecesAndObjs Rpiece id (ownedPtr: ptr) (pieceOut: nat->bool) : Rep :=
+   ([∗ list] ctid ∈ allContenderIds,
+     if pieceOut ctid
+     then pureR (ownedPtr |-> Rpiece ctid)
+          ** SharedPtrR "int" id Rpiece ownedPtr
+     else pureR (copyConstrRight id ctid)).
+
+  Lemma redistributePayloadOwnership {Rpieceold Rpiecenew: nat -> Rep} (pieceOut : nat -> bool) id ownedPtr:
+    allPiecesAndObjs Rpieceold id ownedPtr pieceOut
+      |-- allPiecesAndObjs Rpiecenew id ownedPtr pieceOut.
+  Proof. Admitted.
 
   cpp.spec "testnew4()" as testnew4spec with (
     \pre emp
     \post{p:ptr}[Vptr p] Exists payload sid,
-       p |-> SharedPtrR "int" sid payload
+       p |-> SharedPtrR "int" sid (fun ctid => if bool_decide (ctid=0%nat) then anyR "int" 1 else emp) payload
        ** payload |-> intR (cQp.m 1) 1
-       ** Lstar (repeat (copyConstrRight sid) (Pos.to_nat maxContention - 1)) 
+       ** ([∗ list] ctid ∈ allButFirstContenderId,
+              copyConstrRight sid ctid)
     ).
 
   (** proofs: *)
   Opaque SharedPtrR.
   
-  Definition observeSharedType r q t op:= @observe_fwd _ _ _ (sharedR_typeptr_observe r q t op).
+  Definition observeSharedType r q t Rpiece op:= @observe_fwd _ _ _ (sharedR_typeptr_observe r q t Rpiece op).
 
   Opaque NullSharedPtrR.
   Hint Resolve observeSharedType : br_opacity.
 
+  
+Lemma one_as_bigsep {PROP: bi} {A} {eqd: EqDecision A} (f  : PROP) l (x: A):
+  x ∈ l ->
+  NoDup l -> (* too strong: we only need x to be not duplicated *)
+  f -|- ([∗ list] id ∈ l, if bool_decide (id=x) then f else emp)%I.
+Proof using.
+  clear MOD.
+  intros.
+  rewrite  -> big_op.big_sepL_difference_singleton with (x:=x) by assumption.
+  simpl.
+  case_bool_decide; [ | congruence].
+  assert (f ** (emp)%I ≡ f) as Heq by (apply right_id; eauto with typeclass_instances).
+  rewrite <- Heq at 1.
+  f_equiv.
+  rewrite <- big_sepL_emp with (l:=(list_difference l [x])).
+  apply big_opL_proper.
+  intros  ? id  Hl.
+  case_decide;[ | reflexivity].
+  subst.
+  apply elem_of_list_lookup_2 in Hl.
+  apply elem_of_list_difference in Hl.
+  forward_reason.
+  apply False_rect.
+  simpl in *.
+  set_solver.
+Qed.
+
+Hint Resolve NoDup_seq : setsolver.
+Hint Rewrite elem_of_seq: iff.
   Lemma prf2: verify[module] testnew4spec.
   Proof using MOD.
     verify_spec.
+    pose proof maxContentionLb.
     go.
     unfold dynAllocatedR.
     iExists _.
+    set (Rpiece:=(fun ctid => if bool_decide (ctid=0%nat) then anyR "int" 1 else emp)).
+    iExists Rpiece.
     go.
     iExists _.
     iExists 0.
@@ -192,19 +254,32 @@ Section specs.
     normalize_ptrs.
     eagerUnifyU.
     go.
-    iExists _, _.
+    rewrite <- _at_big_sepL.
+    unfold allButFirstContenderId.
+    erewrite  big_opL_proper with (g := fun _ _=> emp).
+    2:{ intros ? ? Hl. unfold Rpiece.
+        apply elem_of_list_lookup_2 in Hl.
+        autorewrite with iff in Hl.
+        resolveDecide lia.
+        reflexivity.
+    }
+    go.
+    rewrite big_sepL_emp.
+    go.
+    iExists _, _, Rpiece .
     unfold upcast_offset.
     normalize_ptrs.
     eagerUnifyU.
     go.
     normalize_ptrs.
     go.
-    iExists _, _.
+    iExists _, _, Rpiece.
     eagerUnifyU.
     go.
     iExists true.
     iExists nullptr.
     iExists tt.
+    iExists Rpiece.
     ego.
   Qed.
   
